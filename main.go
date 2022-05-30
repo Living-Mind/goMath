149a80a2767bb4978d3244ecea5b65ea73addfec
Living-Mind <redlined_twick@aleeas.com>
2022-05-30 13:31:19 +0300

    New Option: Custom Numbers

--- main.go
+++ main.go
@@ -16,10 +16,12 @@ var answer int
 var end int
 var correctAnswer int
 var customTime int
+var sliceNumbers []int
+var Option6 bool
 
 func main() {
 	customTime = 30
-	fmt.Println("Math Trainer\n--------------------------")
+	fmt.Println("Math Trainer")
 	modeSelect()
 }
 
@@ -85,7 +87,8 @@ func modeSelect() {
 
 	var selection int
 
-	fmt.Println("Select Mode Type:\n 1)Addition\n 2)Subtraction\n 3)Multiplication\n 4)Division\n\nOther Options:\n 5)Set custom time (default time = 30s)")
+	fmt.Println("--------------------------")
+	fmt.Println("Select Mode Type:\n 1)Addition\n 2)Subtraction\n 3)Multiplication\n 4)Division\n\nOther Options:\n 5)Set custom time (default time = 30s)\n 6)Set custom numbers (default numbers = from 2 to 9) *This option works only for Multiplication & Division*")
 	fmt.Scan(&selection)
 	fmt.Println("--------------------------")
 
@@ -102,11 +105,16 @@ func modeSelect() {
 	case 4:
 		fmt.Println("Mode: Division")
 		loopOperation(4)
+		//custom time
 	case 5:
 		fmt.Println("Insert custom time (in seconds):")
 		fmt.Scan(&customTime)
 		fmt.Printf("Time set to %v sec.\n", customTime)
 		modeSelect()
+		//custom numbers
+	case 6:
+		Option6 = true
+		setCustomNumbers()
 	default:
 		fmt.Println("Not an option.")
 		modeSelect()
@@ -123,15 +131,26 @@ func randomizer(modeNum int) {
 		// Value Y (addition || subtraction)
 		numberY = rand.Intn(100)
 	} else {
-		var xArray = [8]int{2, 3, 4, 5, 6, 7, 8, 9}
 
-		// Value X (miltiplication || division)
-		var randValue int = rand.Intn(len(xArray))
-		numberX = xArray[randValue]
+		if !Option6 {
+			var xArray = [8]int{2, 3, 4, 5, 6, 7, 8, 9}
 
-		// Value Y (miltiplication || division)
-		var randValueY int = rand.Intn(len(xArray))
-		numberY = xArray[randValueY]
+			//Using Array for Value X (miltiplication || division)
+			var randValue int = rand.Intn(len(xArray))
+			numberX = xArray[randValue]
+
+			//Using Array for Value Y (miltiplication || division)
+			var randValueY int = rand.Intn(len(xArray))
+			numberY = xArray[randValueY]
+		} else {
+			// Using Slice for Value X (miltiplication || division)
+			var randValue int = rand.Intn(len(sliceNumbers))
+			numberX = sliceNumbers[randValue]
+
+			// Using Slice for Value Y (miltiplication || division)
+			var randValueY int = rand.Intn(len(sliceNumbers))
+			numberY = sliceNumbers[randValueY]
+		}
 	}
 }
 
@@ -186,4 +205,22 @@ func createScoreFile(i int) {
 		fmt.Println("error")
 	}
 	defer scoreFile.Close()
+}
+
+// Slice for Custom Numbers
+func setCustomNumbers() {
+	var customNum int = 1
+
+	for customNum != 0 {
+		fmt.Println("Insert a custom number. When you are setisfied with the added numbers insert 0 to continue:")
+		fmt.Scan(&customNum)
+		if customNum != 0 {
+			sliceNumbers = append(sliceNumbers, customNum)
+			fmt.Printf("Selected custom numbers: %v \n", sliceNumbers)
+		} else {
+			fmt.Printf("The following numbers are set: %v\n", sliceNumbers)
+			modeSelect()
+		}
+	}
+
 }
